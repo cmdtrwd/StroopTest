6d8ddb0d7c023cb38951da12f8d6fa1a16e412a2
JitPom <jittrapol@gmail.com>
2023-01-10 11:51:21 +0700

    Keyboard event

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ let count = 0;
 let pushInterval;
 let isEnabled = true;
 
+let keyPressInterval;
+let isKeyEnabled = true;
+
 function NotifyPush() {
 	// alert("Push!");
     count++;
@@ -25,3 +28,33 @@ function DeactivatePush(){
     console.log(box);
     box.classList.toggle("active");
 }
+
+function DeactivateKey(){
+    document.getElementById("keyA").classList.remove("active");
+    document.getElementById("keyL").classList.remove("active");
+    isKeyEnabled = true;
+    clearInterval(keyPressInterval);
+}
+
+document.addEventListener('keypress', (event)=>{
+    let key_press = event.key;
+    let key_code = event.code;
+
+    console.log("key pressed: " + key_press + ", key code: " + key_code);
+
+    if(key_press == "l"){
+        if (isKeyEnabled){
+            document.getElementById("keyL").classList.add("active");            
+            isKeyEnabled = false;
+            keyPressInterval = setInterval(DeactivateKey, 200);
+        }
+    }
+    else if (key_press == "a"){
+        if (isKeyEnabled){
+            document.getElementById("keyA").classList.add("active");            
+            isKeyEnabled = false;
+            keyPressInterval = setInterval(DeactivateKey, 200);
+        }
+    }
+
+}, false);
