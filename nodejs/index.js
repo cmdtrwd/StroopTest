7d054f27d5d0151d173877a18c2e89006869707b
JitPom <jittrapol@gmail.com>
2023-02-09 13:15:20 +0700

    Promise with then, catch and finally

--- nodejs/index.js
+++ nodejs/index.js
@@ -21,14 +21,23 @@ const connect = false; // Check if there is an Internet connection
 const url1 = "myserver.com/fileA.json";
 
 function Downloading(url){
-    return new Promise(function(resolve, reject){
-        if (connect){
-            console.log("Successfully downloaded " + url);            
-        }
-        else{
-            console.log("Something went wrong");
-        }
+    return new Promise(function(resolve, reject){   
+        console.log("Downloading " + url)     ;
+        setTimeout(()=>{
+            if (connect){
+                resolve("Successfully downloaded " + url);            
+            }
+            else{
+                reject("Something went wrong");
+            }
+        }, 3000);
     });
 }
 
-Downloading(url1);
+Downloading(url1).then(result=>{
+    console.log(result);
+}).catch(err=>{
+    console.log(err);
+}).finally(()=>{
+    console.log("end of process");
+});
