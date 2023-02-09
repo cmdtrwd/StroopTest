b7cc23345a945a292fa8252ccd72742c082f58ab
JitPom <jittrapol@gmail.com>
2023-02-09 13:07:57 +0700

    Promise

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,13 +1,34 @@
-function AddValues(x,y, callback){
-    console.log("calculating...");
-    setTimeout(()=>{
-        const sum = x + y;
-        callback(sum);
-    }, 3000);
+// Call back function
+
+// function AddValues(x,y, callback){
+//     console.log("calculating...");
+//     setTimeout(()=>{
+//         const sum = x + y;
+//         callback(sum);
+//     }, 3000);
+// }
+
+// function Display(result){
+//     console.log('sum = ' + result);
+// }
+
+// AddValues(100, 50, Display);
+
+
+// Promise
+
+const connect = false; // Check if there is an Internet connection
+const url1 = "myserver.com/fileA.json";
+
+function Downloading(url){
+    return new Promise(function(resolve, reject){
+        if (connect){
+            console.log("Successfully downloaded " + url);            
+        }
+        else{
+            console.log("Something went wrong");
+        }
+    });
 }
 
-function Display(result){
-    console.log('sum = ' + result);
-}
-
-AddValues(100, 50, Display);
+Downloading(url1);
