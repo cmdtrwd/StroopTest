d47956f2ff3e5fb76241647c0dbb477773891db5
cmdtrwd <jittrapol.i@psu.ac.th>
2023-02-10 15:53:52 +0700

    Updated the instruction workflow

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ let waitingPeriod = 500;        //Waiting period in milliseconds
 
 let keyPressInterval;
 let isKeyEnabled = true;
+let isInstructionDisplayed = true;
 
 // Define variables for retrieving file information
 let fileName; 
@@ -20,6 +21,8 @@ let maxTrials = 50;             //Number of maximum trials to run
 let images = []; 
 let currentImageNo = 0;
 let probabilityRatioMale = 0.7; //A probability ratio to generate male over female images
+let instructions = [];
+let currentInstructionNo = 0;
 
 // Function to set the timer for the push button
 // NOT IN USE
@@ -46,6 +49,15 @@ function DeactivatePush(){
     console.log(box);
     box.classList.toggle("active");
 }*/
+
+// Function to initialise instructions
+function InitInstructions(){
+    instructions[0] = "The test consists of displaying a series of face images and word names, either in congruent (the word name matches the face image) or incongruent (the word name does not match the face image) pairs.";
+    instructions[1] = "You will be asked to quickly name the emotion displayed on the face image.";
+    instructions[2] = "You should try to ignore the word name and focus only on the face image.";
+    instructions[3] = "The test will repeat for a set number of trials or until the participant chooses to stop.";
+}
+
 
 // Function to re-enable the keys after a specified waiting period
 function DeactivateKey(){
@@ -94,6 +106,21 @@ function DisplayImage(imageNo){
     }
 }
 
+// Function to display instruction in a sequence
+function DisplayInstruction(instructionNo){
+    let instruction = document.getElementById("instruction");
+    instruction.innerHTML = instructions[instructionNo];
+    if (betweenTrialInterval) {
+        clearInterval(betweenTrialInterval);
+        DeactivateKey();
+    }
+}
+
+function InstructionTransition(){
+    betweenTrialInterval = setInterval(DisplayInstruction, waitingPeriod, currentInstructionNo);
+    
+}
+
 // Function to make the screen blank during a waiting period before showing the next image in a sequence
 function TrialTransition(){
     let currentImage = document.getElementById("currentImage");
@@ -102,43 +129,84 @@ function TrialTransition(){
     betweenTrialInterval = setInterval(DisplayImage, waitingPeriod, currentImageNo);    
 }
 
+
+
 // Generate a sequence of images on load
 document.onload = GetFileInfo();
 // Display the first image in a sequence on load
-document.onload = DisplayImage(currentImageNo);
-
+// document.onload = DisplayImage(currentImageNo);
+document.onload = InitInstructions();
+document.onload = DisplayInstruction(currentInstructionNo);
 
 // Detecting the key press event as a response from a participant and later display the next image in a sequence
 document.addEventListener('keypress', (event)=>{
     let key_press = event.key;
     let key_code = event.code;
 
-    
-    if(key_press == "l"){
-        if (isKeyEnabled){
-            let current_key = document.getElementById("keyL");
-            current_key.classList.add("active");            
-            isKeyEnabled = false;            
-            console.log("key pressed: " + current_key.dataset.key + ", key code: " + key_code);
-            currentImageNo++;
-            if (currentImageNo < maxTrials)
-            {
-                TrialTransition();
+    if (!isInstructionDisplayed){
+        if(key_press == "l"){
+            if (isKeyEnabled){
+                let current_key = document.getElementById("keyL");
+                current_key.classList.add("active");            
+                isKeyEnabled = false;            
+                console.log("key pressed: " + current_key.dataset.key + ", key code: " + key_code);
+                currentImageNo++;
+                if (currentImageNo < maxTrials)
+                {
+                    TrialTransition();
+                }
             }
         }
+        else if (key_press == "a"){
+            if (isKeyEnabled){
+                let current_key = document.getElementById("keyA");
+                current_key.classList.add("active");            
+                isKeyEnabled = false;            
+                console.log("key pressed: " + current_key.dataset.key + ", key code: " + key_code);
+                currentImageNo++;
+                if (currentImageNo < maxTrials)
+                {
+                    TrialTransition();
+                }
+            }
+        }  
     }
-    else if (key_press == "a"){
-        if (isKeyEnabled){
-            let current_key = document.getElementById("keyA");
-            current_key.classList.add("active");            
-            isKeyEnabled = false;            
-            console.log("key pressed: " + current_key.dataset.key + ", key code: " + key_code);
-            currentImageNo++;
-            if (currentImageNo < maxTrials)
-            {
-                TrialTransition();
+    else{
+        if (key_press == "l") {
+            if (isKeyEnabled) {
+                let current_key = document.getElementById("keyL");
+                current_key.classList.add("active");
+                isKeyEnabled = false;
+                console.log("key pressed: " + current_key.dataset.key + ", key code: " + key_code);
+                currentInstructionNo++
+                if (currentInstructionNo < 4){
+                    InstructionTransition();
+                }
+                else {
+                    isInstructionDisplayed = false;
+                    document.getElementById("instruction").classList.remove("active");
+                    DisplayImage(currentImageNo);
+                }
             }
         }
-    }  
+        else if (key_press == "a") {
+            if (isKeyEnabled) {
+                let current_key = document.getElementById("keyA");
+                current_key.classList.add("active");
+                isKeyEnabled = false;
+                console.log("key pressed: " + current_key.dataset.key + ", key code: " + key_code);
+                currentInstructionNo++
+                if (currentInstructionNo < 3) {
+                    InstructionTransition();
+                }
+                else{
+                    isInstructionDisplayed = false;
+                    document.getElementById("instruction").classList.remove("active");
+                    DisplayImage(currentImageNo);
+                }
+            }
+        }  
+
+    }
 
 }, false);
