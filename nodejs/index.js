fee36ab2079fa510fc098b5baa449d45653303a9
cmdtrwd <jittrapol.i@psu.ac.th>
2023-02-10 10:54:39 +0700

    somechanges

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,13 +1,31 @@
-function AddValues(x,y, callback){
-    console.log("calculating...");
+// Call Back Functions
+// function AddValues(x,y, callback){
+//     console.log("calculating...");
+//     setTimeout(()=>{
+//         const sum = x + y;
+//         callback(sum);
+//     }, 3000);
+// }
+
+// function Display(result){
+//     console.log('sum = ' + result);
+// }
+
+// AddValues(100, 50, Display);
+
+
+
+const url1= "jptechnote.com/file.json";
+
+function downloading(url, callback){
+    console.log("downloading..." + url);
     setTimeout(()=>{
-        const sum = x + y;
-        callback(sum);
+        callback(url);
     }, 3000);
 }
 
-function Display(result){
-    console.log('sum = ' + result);
+function complete(result){
+    console.log("downloaded " + result + " successfully");
 }
 
-AddValues(100, 50, Display);
+downloading(url1, complete);
